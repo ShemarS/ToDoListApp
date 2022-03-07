be0bccc5d7f095b65718b7c44833eb4b89bf26e9
ShemarS <42276240+ShemarS@users.noreply.github.com>
2022-03-07 14:52:52 -0500

    added both activities created UI
    YUHHH

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.todoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val toDoContents = listOf<String>()
+        val toDoContentsAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, toDoContents)
+        toDoListView.adapter = toDoContentsAdapter
+
     }
+
+    fun addToDoOnClick (view: View) {
+        val myIntent = Intent(this, MainActivity2::class.java)
+        startActivity(myIntent)
+    }
+
+
 }
--- app/src/main/java/com/example/todoapp/MainActivity2.kt
+++ app/src/main/java/com/example/todoapp/MainActivity2.kt
@@ -1,11 +1,29 @@
 package com.example.todoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity2 : AppCompatActivity() {
+    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
     }
+
+    fun saveAddNewOnClick(view: View) {
+
+    }
+
+    fun saveGoBackOnClick(view: View) {
+        val myIntent = Intent(this, MainActivity::class.java)
+        startActivity(myIntent)
+    }
+
+    fun goBackWithoutSaveOnClick(view: View) {
+        val myIntent = Intent(this, MainActivity::class.java)
+        startActivity(myIntent)
+    }
+
 }
