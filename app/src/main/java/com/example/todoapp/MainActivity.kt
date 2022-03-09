b1c4089886cbf51fc3a8b49f2e589066f6b9a028
ShemarS <42276240+ShemarS@users.noreply.github.com>
2022-03-09 00:04:06 -0500

    almost done?
    need to figure out how to move json data to main activity

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -1,27 +1,63 @@
 package com.example.todoapp
 
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
+const val TAG = "FirstActivity"
+
+
 class MainActivity : AppCompatActivity() {
+    val toDoContents = listOf<String>().toMutableList()
+    val REQUEST_CODE = 414
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
 
-        val toDoContents = listOf<String>()
+    override fun OnStart () {
+        super.onStart()
         val toDoContentsAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, toDoContents)
         toDoListView.adapter = toDoContentsAdapter
 
+        toDoListView.setOnItemLongClickListener { parent, view, position, id ->
+            val selectedItem = parent.getItemAtPosition(position).toString()
+            Toast.makeText(this, "Removing $selectedItem", Toast.LENGTH_SHORT).show()
+            toDoContents.removeAt(position)
+            toDoContentsAdapter.notifyDataSetChanged()
+            return@setOnItemLongClickListener true
+        }
     }
 
     fun addToDoOnClick (view: View) {
         val myIntent = Intent(this, MainActivity2::class.java)
-        startActivity(myIntent)
+        startActivityForResult(myIntent, REQUEST_CODE)
+    }
+
+    /*
+        override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume was called")
+    }
+    */
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE){
+                // extract the data from the intent
+            val task = data?.getStringExtra("task")
+            if (task != null) {
+                toDoContents.add(task)
+            }
+        }
     }
 
 
--- app/src/main/java/com/example/todoapp/MainActivity2.kt
+++ app/src/main/java/com/example/todoapp/MainActivity2.kt
@@ -1,14 +1,21 @@
 package com.example.todoapp
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_main2.*
+import java.util.ArrayList
 
 class MainActivity2 : AppCompatActivity() {
     var count = 0
+    val toDoContents = listOf<String>().toMutableList()
+    val gson = Gson()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
@@ -20,13 +27,27 @@ class MainActivity2 : AppCompatActivity() {
             Toast.makeText(this, "Please add a task", Toast.LENGTH_SHORT).show()
             return
         }
-        
+/*        val secondIntent = Intent(this, MainActivity:: class.java)
+        secondIntent.putExtra("task", task)*/
 
+
+        toDoContents.add(task)
+        val jsonString = gson.toJson(toDoContents)
+
+        val sType = object : TypeToken<List<String>>() { }.type
+        val myList = gson.fromJson<List<String>>(jsonString, sType)
+
+        Log.d(TAG, "$myList")
+
+        val myIntent = Intent()
+        myIntent.putStringArrayListExtra("task", myList as ArrayList<String>?)
+        setResult(Activity.RESULT_OK, myIntent)
+        addTaskEditText.text.clear()
     }
 
     fun saveGoBackOnClick(view: View) {
-        val myIntent = Intent(this, MainActivity::class.java)
-        startActivity(myIntent)
+        val secondIntent = Intent(this, MainActivity::class.java)
+        startActivity(secondIntent)
     }
 
     fun goBackWithoutSaveOnClick(view: View) {
